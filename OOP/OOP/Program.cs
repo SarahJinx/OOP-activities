de620e4cc890715c0482ed706adda2b9fa3f42d5
Sarah <scartleha1123@gmail.com>
2025-03-24 17:19:22 +0100

    Ui and replayability
    Made a program class with a main, it contains your current captured pokemons.
    
    it shows u a random pokemon and you can C catch it or R run, if u run it shows current inventory and then repeas, if u catch there is a 50% chance u catch it, then shows then repeats

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -84,4 +84,57 @@
     {
         public Sylveon() : base("Sylveon", "Fairy", 95, 65, 65, 60) { }
     }
+
+    class Program
+    {
+        static List<Eevee> capturedPokemons = new List<Eevee>();
+        static Random random = new Random();
+        static Eevee[] pokemonsInInventory =
+        {
+            new BaseEevee(), new Vaporeon(), new Jolteon(), new Flareon(),
+            new Espeon(), new Umbreon(), new Leafeon(), new Glaceon(), new Sylveon()
+        };
+
+        static void Main()
+        {
+            while (true)
+            {
+                Eevee wildPokemon = pokemonsInInventory[random.Next(pokemonsInInventory.Length)];
+                Console.WriteLine($"A wild {wildPokemon.Name} appeared! It says: {wildPokemon.MakeSound()}");
+                Console.WriteLine("What will you do? (C) Catch (R) Run");
+                char choice = char.Parse(Console.ReadLine());
+                if (choice == 'C')
+                {
+                    if (random.Next(0, 2) == 1)
+                    {
+                        Console.WriteLine($"You caught {wildPokemon.Name}!");
+                        capturedPokemons.Add(wildPokemon);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{wildPokemon.Name} escaped!");
+                    }
+                }
+                else if (choice == 'R')
+                {
+                    Console.WriteLine("You ran away!");
+                }
+                else
+                {
+                    Console.WriteLine("You had a stroke, forgot to breathe and died tragically.");
+                    return;
+                }
+
+                Console.WriteLine("Captured Pokémon:");
+                foreach (var pokemon in capturedPokemons)
+                {
+                    Console.WriteLine($"- {pokemon.Name} | Type: {pokemon.Type} | HP: {pokemon.HP} | Attack: {pokemon.Attack}" +
+                                 " | Defense: {pokemon.Defense} | Speed: {pokemon.Speed} | It says: {wildPokemon.MakeSound()}");
+                }
+                Console.WriteLine("Press any key to keep on your quest catch them all! >:3");
+                Console.ReadKey();
+                Console.Clear();    
+            }
+        }
+    }
 }
